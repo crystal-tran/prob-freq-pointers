7db01724feb5cf1bdd5b8c23fe6dd9533e3e17ff
Victor Kim <vskimjr@gmail.com>
2023-11-10 19:44:12 -0500

    averagePair completed

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,30 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/** averagePair
+ * checks each unique pair in the sortedNums to see if the average
+ * of those pairs match the targetAvg. Returns corresponding boolean
+*/
+
+function averagePair(nums, targetAvg){ // a better name for nums would be sortedNums
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right){
+    const avg = (nums[left] + nums[right]) / 2;
+    if (avg === targetAvg){
+      return true;
+    } else if (avg < targetAvg){
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return false;
 }
 
+// [1,2,3,4,5,6] targetAvg = 4.5
+
+// first 1,6 = 3.5
+// if we move the left, 2,6 = 4
+// if we move the right, 1,5 = 3
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,12 +1,23 @@
+'use strict';
 // add whatever parameters you deem necessary & write doc comment
 //"aabc"  "bbaac"
+
+/**
+ *
+ * what does this do?
+ * canConstructWord accepts a word and letters as its parameters. It should
+ * return true if the word can be built with the letters; otherwise,
+ *  it should return false.
+ */
+
+
 function canConstructWord(word, letters) {
   if (letters.length === 0) {
     return false;
   }
 
   let word1Freq = getFrequency(word);
-  let lettersFreq = getFrequency(letters);
+  let lettersFreq = getFrequency(letters); // change these to const. line 9 too
   console.log("word1Freq", word, word1Freq);
   console.log("lettersFreq", letters, lettersFreq);
   for (let key in word1Freq) {
