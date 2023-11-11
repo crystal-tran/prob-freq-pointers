afa2362cf2b8cff6cece2befab4c96810f1b0deb
Crystal Tran <c.t.tran95@gmail.com>
2023-11-10 20:01:34 -0500

    completed twoArrayObject

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,19 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+/**Given 2 positive integers, return a true if they have the same frequency
+ * otherwise return false
+ */
+function sameFrequency(num1, num2) {
+
+  let num1Frequency = getFrequency(num1);
+}
+
+function getFrequency(num){
+  let numString = num.toString();
+  let numToFrequency = {};
+  for (let digit of numString) {
+    const count = (numToFrequency[digit] || 0) + 1;
+    numToFrequency[digit] = count;
+    }
+return numToFrequency
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,17 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**This function takes in two arrays of possibly different lengths
+ * First array contains keys. Second array contains values
+ * Return an object created from the keys and values
+ * If there are not enough keys, ignore the rest of values
+ * If there are not enough values, the rest of the keys have a value of null
+ */
+function twoArrayObject(arr1, arr2) {
+  let result = {};
+
+  for(let i=0; i < arr1.length; i++){
+    const value = arr2[i] || null;
+    result[arr1[i]] = value;
+  }
+  return result;
 }
