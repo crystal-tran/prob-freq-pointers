b39fc1cd06b667d416effc4e6f39eb0de2d316ed
Crystal Tran <c.t.tran95@gmail.com>
2023-11-10 19:22:00 -0500

    first problem done

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,35 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+//"aabc"  "bbaac"
+function canConstructWord(word, letters) {
+  if (letters.length === 0) {
+    return false;
+  }
+
+  let word1Freq = getFrequency(word);
+  let lettersFreq = getFrequency(letters);
+  console.log("word1Freq", word, word1Freq);
+  console.log("lettersFreq", letters, lettersFreq);
+  for (let key in word1Freq) {
+    if (lettersFreq[key] === undefined) {
+      return false;
+    } else {
+      if (word1Freq[key] > lettersFreq[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+
+function getFrequency(word) {
+  let letterCount = {};
+
+  for (let letter of word) {
+    const count = (letterCount[letter] || 0) + 1;
+    letterCount[letter] = count;
+  }
+
+  return letterCount;
 }
